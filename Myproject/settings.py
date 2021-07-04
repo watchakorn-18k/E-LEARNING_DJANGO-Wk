52ac9d2240f1a0cc974287e4229070301aca4b5b
watchakorn-18k <porton2559@gmail.com>
2021-07-04 11:21:56 +0700

    Deploy

--- Myproject/settings.py
+++ Myproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -28,7 +29,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['https://django-e-learning.herokuapp.com/']
 
 
-# Application definition
+# Application definitio
 
 INSTALLED_APPS = [
     'django.contrib.admin',
