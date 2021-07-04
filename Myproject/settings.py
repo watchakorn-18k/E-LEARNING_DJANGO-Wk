20c1cc5512298a794a9bfe93598c690111ca31d9
watchakorn-18k <porton2559@gmail.com>
2021-07-04 13:44:20 +0700

    update

--- Myproject/settings.py
+++ Myproject/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,3 +132,4 @@ STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'MyWebsite/static')]
 STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path.joinpath(BASE_DIR, 'MyWebsite/static/media')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
