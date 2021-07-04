5a2df38bf18baf8f736d260029290c26dd6b4b32
watchakorn-18k <porton2559@gmail.com>
2021-07-04 11:13:24 +0700

    update host

--- Myproject/settings.py
+++ Myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kn$=sg5^*4v@1qp_+^h-+g&)l2tjrme_=ea8xtcyc9nuwufy#s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://django-e-learning.herokuapp.com/']
 
 
 # Application definition
